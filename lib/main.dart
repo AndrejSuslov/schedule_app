2977d5ae8e7acba9f731c8a674e40d9b8665b452
Vlad Kirov <wladponomar@gmail.com>
2023-09-04 16:19:12 +0300

    added theme. needs to be refactored

--- lib/main.dart
+++ lib/main.dart
@@ -1,44 +1,64 @@
 import 'package:flutter/material.dart';
 
 void main() {
-  runApp(MyApp());
+  runApp(const MyApp());
 }
 
 class MyApp extends StatelessWidget {
+  static final ValueNotifier<ThemeMode> themeNotifier =
+      ValueNotifier(ThemeMode.light);
+
+  const MyApp({Key? key}) : super(key: key);
+
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-        theme: ThemeData(primaryColor: Colors.red),
-        home: Scaffold(
-          appBar: AppBar(
-            title: const Text('some shiiiiiit'),
-            //const Icon(Icons.access_alarm_sharp),
-            centerTitle: true,
-          ),
-          body: Row(
-            mainAxisAlignment: MainAxisAlignment.spaceAround,
-            children: [
-              Column(
-                children: [
-                  Text('some'),
-                  TextButton(onPressed: () {}, child: Text('Presss me')),
-                ],
-              ),
-              Column(
-                children: [
-                  Text('some'),
-                  TextButton(onPressed: () {}, child: Text('Presss me')),
-                ],
-              )
-            ],
-          ),
-          floatingActionButton: FloatingActionButton(
-            child: const Text('Press me'),
-            backgroundColor: Colors.black,
-            onPressed: () {
-              print('You clicked');
-            },
-          ),
-        ));
+    return ValueListenableBuilder<ThemeMode>(
+        valueListenable: themeNotifier,
+        builder: (_, ThemeMode currentMode, __) {
+          return MaterialApp(
+            // Remove the debug banner
+            debugShowCheckedModeBanner: false,
+            title: 'Andron broke',
+            theme: ThemeData(primarySwatch: Colors.amber),
+            darkTheme: ThemeData.dark(),
+            themeMode: currentMode,
+            home: const HomeScreen(),
+          );
+        });
+  }
+}
+
+// Home Screen
+class HomeScreen extends StatelessWidget {
+  const HomeScreen({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('some text it will be better soon'),
+        actions: [
+          IconButton(
+              icon: Icon(MyApp.themeNotifier.value == ThemeMode.light
+                  ? Icons.dark_mode
+                  : Icons.light_mode),
+              onPressed: () {
+                MyApp.themeNotifier.value =
+                    MyApp.themeNotifier.value == ThemeMode.light
+                        ? ThemeMode.dark
+                        : ThemeMode.light;
+              })
+        ],
+      ),
+      body: Center(
+        child: ElevatedButton(
+          child: const Text('Button'),
+          onPressed: () {
+            // Navigator.push(context,
+            //     MaterialPageRoute(builder: (context) => const OtherScreen()));
+          },
+        ),
+      ),
+    );
   }
 }
