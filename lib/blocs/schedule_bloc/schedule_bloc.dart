06d5708a645d4ba5b41d54de17d151919efeaf91
Androoon <andrejsuslov308@gmail.com>
2023-12-24 15:03:02 +0300

    for new branch

--- lib/blocs/schedule_bloc/schedule_bloc.dart
+++ lib/blocs/schedule_bloc/schedule_bloc.dart
@@ -8,12 +8,13 @@ import 'package:flutter_test_project/services/events.dart';
 import 'package:flutter_test_project/services/parser.dart';
 import 'package:intl/intl.dart';
 import 'package:sqflite/sqflite.dart';
+import 'package:table_calendar/table_calendar.dart';
 
 part 'schedule_event.dart';
 part 'schedule_state.dart';
 
 class ScheduleBloc extends Bloc<ScheduleEvent, ScheduleState> {
-  late DateTime currentDay;
+  late DateTime currentDay = DateTime.now();
   late final Map<DateTime, List<String>> loadedClassesForFirstGroup;
 
   ScheduleBloc() : super(ScheduleInitial()) {
@@ -21,15 +22,20 @@ class ScheduleBloc extends Bloc<ScheduleEvent, ScheduleState> {
       (event, emit) {},
     );
     on<ChangeDateOfClasses>(_onChangeDate);
-    on<LoadScheduleFromFile>(_loadScheduleFromFile); // Добавим новое событие
+    //on<LoadScheduleFromFile>(_loadScheduleFromFile); // Добавим новое событие
     _loadSchedule();
   }
 
   void _loadSchedule() async {
     emit(ScheduleLoading());
     final parsedExcel = ExcelParsing(6, 2);
+    await parsedExcel.parseForAllGroups();
     {
       loadedClassesForFirstGroup = parsedExcel.getClassesForChoosedGroup(1);
+      if (currentDay.weekday == 7) {
+        emit(
+            /* here will be emit to the state void day with animation */ somevoiddaystate);
+      }
       emit(ScheduleLoaded(
           loadedClassesForFirstGroup[currentDay]!.toList(), currentDay));
     }
@@ -52,7 +58,7 @@ class ScheduleBloc extends Bloc<ScheduleEvent, ScheduleState> {
   FutureOr<void> _loadScheduleFromFile(
       LoadScheduleFromFile event, Emitter<ScheduleState> emit) async {
     final parsedExcel = ExcelParsing(6, 2);
-    // final data = await parsedExcel.parseForAllGroups();
+    await parsedExcel.parseForAllGroups();
     loadedClassesForFirstGroup = parsedExcel.getClassesForChoosedGroup(1);
     emit(ScheduleLoaded(
         loadedClassesForFirstGroup[currentDay]!.toList(), currentDay));
